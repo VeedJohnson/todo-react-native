82d3c507b801bcf6c007901a9a298ce005398b80
VeedJohnson <48daveed@gmail.com>
2022-09-21 15:33:55 +0100

    refactored addTodo function

--- store/TodosContext.js
+++ store/TodosContext.js
@@ -20,17 +20,20 @@ const TodosProvider = ({children}) => {
             isCompleted: isCompleted,
             id: id
         };
+
         setTodos(currTodos => [todoDetail, ...currTodos]);
+        
+        // const todosRaw = await AsyncStorage.getItem('MY_TODOS');
+        
+        // let todoStore;
+        // if(todosRaw) {
+        //     const todosParsed = JSON.parse(todosRaw);
+        //     todoStore = JSON.stringify([todoDetail, ...todosParsed]);
+        // } else {
+            //     todoStore = JSON.stringify([todoDetail]);
+            // }
 
-        const todosRaw = await AsyncStorage.getItem('MY_TODOS');
-
-        let todoStore;
-        if(todosRaw) {
-            const todosParsed = JSON.parse(todosRaw);
-            todoStore = JSON.stringify([todoDetail, ...todosParsed]);
-        } else {
-            todoStore = JSON.stringify([todoDetail]);
-        }
+        const todoStore = JSON.stringify(todos);
         await AsyncStorage.setItem('MY_TODOS', todoStore);
     };
 
